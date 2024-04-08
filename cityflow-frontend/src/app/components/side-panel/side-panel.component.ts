679e9bfd40ca25799c2c8f46ab0c27a8ccd43823
Nemanja Todorović <77896776+Nemkac@users.noreply.github.com>
2024-04-08 14:39:33 +0200

    Side pannel elements

--- cityflow-frontend/src/app/components/side-panel/side-panel.component.ts
+++ cityflow-frontend/src/app/components/side-panel/side-panel.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { SigninSignupComponent } from '../signin-signup/signin-signup.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser, faHome, faIdCard, faInbox } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [SigninSignupComponent],
+  imports: [SigninSignupComponent,FontAwesomeModule],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.css'
 })
 export class SidePannelComponent {
-
+  //Icons
+  faUser = faUser;
+  faHome = faHome;
+  faIdCard = faIdCard;
+  faInbox = faInbox;
 }
