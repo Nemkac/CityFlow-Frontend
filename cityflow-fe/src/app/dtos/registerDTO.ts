402f6693594c854a43199478490c658528441abe
Nemanja Todorović <77896776+Nemkac@users.noreply.github.com>
2024-04-14 17:58:07 +0200

    Login and register fix

--- cityflow-fe/src/app/dtos/registerDTO.ts
+++ cityflow-fe/src/app/dtos/registerDTO.ts
@@ -2,5 +2,8 @@ export interface RegisterDTO{
     username: string,
     name: string,
     lastname: string,
-    password: string
+    password: string,
+    email: string,
+    phone: string,
+    dateOfBirth: string,
 }
