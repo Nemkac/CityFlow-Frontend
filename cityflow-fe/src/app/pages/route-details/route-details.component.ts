3c6edfeceec14c769bb7a7fc057dea0c4acac2aa
Nemkac <nemanjatodorovic132002002@gmail.com>
2024-05-06 14:43:32 +0200

    Display route

--- cityflow-fe/src/app/pages/route-details/route-details.component.ts
+++ cityflow-fe/src/app/pages/route-details/route-details.component.ts
@@ -6,6 +6,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBus, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import * as L from 'leaflet';
+import { Routing } from 'leaflet';
 
 @Component({
   selector: 'app-route-details',
@@ -15,6 +16,8 @@ import * as L from 'leaflet';
   styleUrl: './route-details.component.css'
 })
 export class RouteDetailsComponent implements OnInit, AfterViewInit{
+  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
+
   //Icons
   faBus = faBus;
   faPen = faPen;
@@ -22,6 +25,9 @@ export class RouteDetailsComponent implements OnInit, AfterViewInit{
 
   routeId : number = 0;
   route!: Route;
+  startingPoint!: L.LatLng;
+  endingPoint!: L.LatLng;
+  stations: L.LatLng[] = []; 
 
   constructor(private routeService: RoutesService,
               private routes: ActivatedRoute){}
@@ -38,6 +44,8 @@ export class RouteDetailsComponent implements OnInit, AfterViewInit{
     this.routeService.getRoute(this.routeId).subscribe(
       (response : Route) => {
         this.route = response;
+        this.fetchStations();
+
         console.log("ROUTE: \n", this.route);
       },
       (error : HttpErrorResponse) => {
@@ -46,7 +54,22 @@ export class RouteDetailsComponent implements OnInit, AfterViewInit{
     )
   }
 
-  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
+  fetchStations() : void {
+    const startingPoint: any = this.route.startingPoint;
+    this.startingPoint = L.latLng(startingPoint.latitude, startingPoint.longitude);
+
+    const endingPoint: any = this.route.startingPoint;
+    this.endingPoint = L.latLng(endingPoint.latitude, endingPoint.longitude);
+
+    const stations: any[] = this.route.stations;
+    this.stations = stations.map(station => L.latLng(station.latitude, station.longitude));
+
+    console.log('Starting station: \n', this.startingPoint);
+    console.log('Ending station: \n', this.endingPoint);
+    console.log('Stations: \n', this.stations);
+  }
+
+
 
   ngAfterViewInit(): void {
     this.loadMap();
@@ -54,11 +77,57 @@ export class RouteDetailsComponent implements OnInit, AfterViewInit{
 
   loadMap() {
     const map = L.map(this.mapContainer.nativeElement).setView(
-      [45.267136, 19.833549],
+      [this.startingPoint.lat, this.startingPoint.lng],
       12
     );
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-    L.marker([45.267136, 19.833549]).addTo(map);
+
+    L.marker([this.startingPoint.lat, this.startingPoint.lng], { icon: this.startingIcon }).addTo(map);
+
+    L.marker([this.endingPoint.lat, this.endingPoint.lng], { icon: this.endingIcon }).addTo(map);
+  
+    this.stations.forEach(station => {
+      L.marker([station.lat, station.lng], { icon: this.busIcon }).addTo(map);
+    });
+
+    const waypoints = [
+      L.latLng(this.startingPoint.lat, this.startingPoint.lng),
+      ...this.stations.map(station => L.latLng(station.lat, station.lng)),
+      L.latLng(this.endingPoint.lat, this.endingPoint.lng)
+    ];
+  
+    const plan = new L.Routing.Plan(waypoints, {
+      createMarker: function() { return false; }
+    });
+  
+    L.Routing.control({
+      plan: plan,
+    }).addTo(map);
   }
+
+  
+  private startingIcon = L.icon({
+    iconUrl: 'assets/flag.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+  
+  private endingIcon = L.icon({
+    iconUrl: 'assets/pin.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+  
+  private busIcon = L.icon({
+    iconUrl: 'assets/bus.png',
+    iconSize: [30, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
 }
